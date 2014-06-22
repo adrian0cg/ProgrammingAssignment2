40026d960548f891e4901244a2f929a4744c4159
Adrian Haß <adrian.hass@googlemail.com>
2014-06-22 11:45:26 +0200

    Added Cached Vector Mean Example

---
+++ cachevector.R
@@ -0,0 +1,31 @@
+## Exmple for calculating a cached mean of a vector
+
+# create datastructure
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+# execute computation/retrieve from cache
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
