fa5f758d037bd97680e7f31e8a9cfbbfe70611fd
Adrian Haß <adrian.hass@googlemail.com>
2014-06-22 12:28:38 +0200

    implemented assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,37 @@
 ## Creates a data structure, that holds the data, the cache and getters/setters for the values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # set the cached value to NULL, indicating invalid cache
+  inv <- NULL
+  # data setter
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL # invalidate cache
+  }
+  get <- function() x
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  list(
+    set = set,
+    get = get,
+    setInv = setInv,
+    getInv = getInv
+  )
 }
 
 
 ## solves the computation possibly using a cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # try retrieve from cache
+  inv <- x$getInv()
+  if (!is.null(inv)) { # HIT!
+    message("retrieving matrix from cache")
+    return(inv)
+  }
+  ## miss
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInv(inv) # store
+  inv
 }
